5babf3708ef7ab6bfece65bb9d230511075c083c
Francisco Tolmasky <tolmasky@francisco-tolmaskys-power-mac-g5.local>
2008-12-06 15:12:05 -0800

    Fix for broken steam with new build system.
    Reviewed by me.

--- Tools/steam/steam.js
+++ Tools/steam/steam.js
@@ -382,30 +382,49 @@ function mainCreate()
 {   
     if (arguments.length < 1)
         printUsage("create");
-    
-    var dst = arguments[0],
-        link = arguments[1] == "-l";
+        
+    var link = false,
+        index = 0,
+        count = arguments.length;
 
-    var srcNewApp       = new File(OBJJ_HOME+"/lib/NewApplication"),
-        srcFrameworks   = new File(OBJJ_HOME+"/lib/Frameworks"),
-        dstNewApp       = new File(dst),
-        dstFrameworks   = new File(dst+"/Frameworks");
-    
-    System.out.println(srcNewApp.getCanonicalPath() + "," + dstNewApp.getCanonicalPath() + "," + srcFrameworks.getCanonicalPath() + "," + dstFrameworks.getCanonicalPath());
-    
-    if (!dstNewApp.exists())
+    for (; index < count; ++index)
     {
-        exec(["cp", "-vR", srcNewApp.getCanonicalPath(), dstNewApp.getCanonicalPath()], true);
+        var argument = arguments[index];
+        
+        switch (arguments[index])
+        {
+            case "-l":  link = true;
+                        break;
+            default:    destination = argument;
+        }
+    }
+
+    var sourceNewApplication = new File(OBJJ_HOME + "/lib/NewApplication"),
+        destinationNewApplication = new File(destination),
+        sourceFrameworks = new File(OBJJ_HOME + "/lib/Frameworks"),
+        sourceDebugFrameworks = new File(OBJJ_HOME + "/lib/Frameworks-Debug"),
+        destinationFrameworks = new File(destination + "/Frameworks"),
+        destinationDebugFrameworks = new File(destination + "/Frameworks-Debug");
+    
+    System.out.println(sourceNewApplication.getCanonicalPath() + "," + destinationNewApplication.getCanonicalPath() + "," + sourceFrameworks.getCanonicalPath() + "," + destinationFrameworks.getCanonicalPath());
+    
+    if (!destinationNewApplication.exists())
+    {
+        exec(["cp", "-vR", sourceNewApplication.getCanonicalPath(), destinationNewApplication.getCanonicalPath()], true);
         
         if (!link)
-            exec(["cp", "-vR", srcFrameworks.getCanonicalPath(), dstFrameworks.getCanonicalPath()], true);            
+        {
+            exec(["cp", "-vR", sourceDebugFrameworks.getCanonicalPath(), destinationDebugFrameworks.getCanonicalPath()], true);
+            exec(["cp", "-vR", sourceFrameworks.getCanonicalPath(), destinationFrameworks.getCanonicalPath()], true);
+        }
         else
-            exec(["ln", "-s", new File(System.getenv("STEAM_BUILD")+"/Release").getCanonicalPath(), dstFrameworks.getCanonicalPath()], true);    
+        {
+            exec(["ln", "-s", new File(System.getenv("STEAM_BUILD") + "/Debug").getCanonicalPath(), destinationDebugFrameworks.getCanonicalPath()], true);
+            exec(["ln", "-s", new File(System.getenv("STEAM_BUILD") + "/Release").getCanonicalPath(), destinationFrameworks.getCanonicalPath()], true);
+        }
     }
     else
-    {
         System.out.println("Directory already exists");
-    }
 }
 
 function mainBuild()
