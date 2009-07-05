9d64526f266d2184ab6b063f002cde3dcb1c1205
Geoffrey Grosenbach <boss@topfunky.com>
2009-07-05 14:59:41 -0700

    Emacs mode for editing Objective-J files.

---
+++ Tools/Editors/Emacs/README.txt
@@ -0,0 +1,32 @@
+Description
+===========
+A simple Emacs mode for editing Objective-J (Cappuccino) files.
+
+Author
+======
+Geoffrey Grosenbach, PeepCode Screencasts 
+http://peepcode.com
+
+Features
+========
+* Syntax highlighting thanks to objc-c-mode.el.
+* Some adherence to Objective-J coding style guidelines (indentation).
+* Automatic loading of objj-mode when .j files are opened.
+
+Installation
+============
+Add objc-c-mode.el and objj-mode.el to your load path and require 
+objj-mode.
+
+  (add-to-list 'load-path "/path/to/objc-c-mode.el")
+  (add-to-list 'load-path "/path/to/objj-mode.el")
+  (require 'objj-mode)
+
+Other
+=====
+If you use yasnippet (http://code.google.com/p/yasnippet/), you can define 
+tab-triggered snippet templates specifically for Objective-J.
+
+Put your snippets in the "snippets/text-mode/objj-mode" directory where
+you keep your other yasnippets.
+
---
+++ Tools/Editors/Emacs/objj-mode.el
@@ -0,0 +1,31 @@
+;;; objj-mode.el --- Major mode for editing Objective-J (Cappuccino) files
+;;; Written by Geoffrey Grosenbach http://peepcode.com
+
+;;; Builds on the excellent objc-mode from http://www.foldr.org/~michaelw/objective-c/objc-c-mode.el
+
+;;; To install, save this somewhere and add the following to your .emacs file:
+;;;
+;;; (add-to-list 'load-path "/path/to/objc-c-mode.el")
+;;; (add-to-list 'load-path "/path/to/objj-mode.el")
+;;; (require 'objj-mode)
+;;;
+;;; Features:
+;;; * Syntax highlighting
+;;; * Indentation (minimal adherence to Obj-J coding style guidelines)
+
+(require 'objc-c-mode)
+
+(define-derived-mode objj-mode objc-mode
+  "Objective-J"
+  "Major mode for editing Objective-J files."
+  (setq c-basic-offset 4)             ;; 4 spaces for tab
+  (setq indent-tabs-mode nil)         ;; Spaces, not tabs
+  (c-set-offset 'substatement-open 0) ;; Curly brace on next line
+  )
+
+;; TODO: Define more syntax settings to comply with http://cappuccino.org/contribute/coding-style.php
+
+(add-to-list 'auto-mode-alist '("\\.j$" . objj-mode))
+
+(provide 'objj-mode)
+
