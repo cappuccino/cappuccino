88635ed4404ea75795dc9769709225bdc0624a59
Tom Robinson <tom@280north.com>
2009-11-11 17:59:01 -0800

    bootstrap.sh changes: reorder shell config checks, add option to set JSC to default, misc cleanup.

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,6 +27,7 @@ INSTALL_DIRECTORY="/usr/local/narwhal"
 ORIGINAL_PATH="$PATH"
 
 if ! which -s "narwhal"; then
+    echo "================================================================================"
     echo "Narwhal JavaScript platform is required. Install it into \"$INSTALL_DIRECTORY\"?"
     if prompt; then
         echo "Downloading Narwhal..."
@@ -53,16 +54,25 @@ if ! which -s "narwhal"; then
 fi
 
 echo "Installing necessary dependencies..."
-tusk install browserjs
-tusk install jake
+tusk install browserjs jake
 
 if [ `uname` = "Darwin" ]; then
-    echo "Would you like to install the JavaScriptCore engine for Narwhal? This is optional but will make building and running Objective-J much faster."
+    echo "================================================================================"
+    echo "Would you like to install the JavaScriptCore engine for Narwhal?"
+    echo "This is optional but will make building and running Objective-J much faster."
     if prompt; then
         tusk install narwhal-jsc
         pushd "$INSTALL_DIRECTORY/packages/narwhal-jsc"
         make webkit
         popd
+        
+        echo "================================================================================"
+        echo "Rhino is the default Narwhal engine, should we change the default to JavaScriptCore for you?"
+        echo "This can by overridden by setting the NARWHAL_ENGINE environment variable to \"jsc\" or \"rhino\"."
+        echo "(Note: you must use Rhino for certain tools such as \"tusk\")"
+        if prompt; then
+            tusk engine jsc
+        fi
     fi
 fi
 
@@ -70,22 +80,24 @@ export PATH="$ORIGINAL_PATH"
 if ! which -s "narwhal"; then
     
     SHELL_CONFIG=""
-    if [ -f "$HOME/.bashrc" ]; then
+    if [ -f "$HOME/.profile" ]; then
+        SHELL_CONFIG="$HOME/.profile"
+    elif [ -f "$HOME/.bashrc" ]; then
         SHELL_CONFIG="$HOME/.bashrc"
     elif [ -f "$HOME/.bash_profile" ]; then
         SHELL_CONFIG="$HOME/.bash_profile"
-    elif [ -f "$HOME/.profile" ]; then
-        SHELL_CONFIG="$HOME/.profile"
     elif [ -f "$HOME/.bash_login" ]; then
         SHELL_CONFIG="$HOME/.bash_login"
     fi
 
-    EXPORT_PATH_STRING="\nexport PATH=\"$INSTALL_DIRECTORY/bin:\$PATH\""
-
+    EXPORT_PATH_STRING="export PATH=\"$INSTALL_DIRECTORY/bin:\$PATH\""
+    
+    echo "================================================================================"
     echo "You must add Narwhal's \"bin\" directory to your PATH environment variable. Do this automatically now?"
     echo "\"$EXPORT_PATH_STRING\" will be appended to \"$SHELL_CONFIG\"."
     if prompt; then
         if [ "$SHELL_CONFIG" ]; then
+            echo >> "$SHELL_CONFIG"
             echo "$EXPORT_PATH_STRING" >> "$SHELL_CONFIG"
             echo "Added to \"$SHELL_CONFIG\". Restart your shell or run \"source $SHELL_CONFIG\"."
         else
