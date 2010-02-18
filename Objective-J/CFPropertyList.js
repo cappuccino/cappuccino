94d3053232aaf3d761639c52007e7954233fb3a1
Francisco Ryan Tolmasky I <francisco@280north.com>
2010-02-18 15:28:30 -0800

    Added CommonJS extensions to CFPropertyList.
    Reviewed by me.

--- Objective-J/CFPropertyList.js
+++ Objective-J/CFPropertyList.js
@@ -60,6 +60,18 @@ CFPropertyList.stringFromPropertyList = function(/*CFPropertyList*/ aPropertyLis
             serializers["finish"]();
 }
 
+#ifdef COMMONJS
+CFPropertyList.readPropertyListFromFile = function(/*String*/ aFilePath)
+{
+    return CFPropertyList.propertyListFromString(FILE.read(aFilePath, { charset:"UTF-8" }));
+}
+
+CFPropertyList.writePropertyListToFile = function(/*CFPropertyList*/ aPropertyList, /*String*/ aFilePath)
+{
+    return FILE.write(aFilePath, CFPropertyList.stringFromPropertyList(aPropertyList), { charset:"UTF-8" });
+}
+#endif
+
 function serializePropertyList(/*CFPropertyList*/ aPropertyList, /*Object*/ serializers)
 {
     var type = typeof aPropertyList,
