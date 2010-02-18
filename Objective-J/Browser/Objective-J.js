d40619aca072134fe41369b161add2d12b1e0a02
Ross Boucher <ross@280north.com>
2010-02-17 16:55:07 -0800

    A better fix for the IE bug. Rather than introduce the secondary global and the eval, just refer to the actual global object (works in any javascript env).

--- Objective-J/Browser/Objective-J.js
+++ Objective-J/Browser/Objective-J.js
@@ -1,27 +1,8 @@
 
-var global = { },
-    ObjectiveJ = { };
+var ObjectiveJ = { };
 
 (function (global, exports, namespace)
 {
-    var GLOBAL_NAMESPACE = namespace;
 #include "Includes.js"
-})(global, ObjectiveJ, window);
+})((function(){return this;}).call(null), ObjectiveJ);
 
-var hasOwnProperty = Object.prototype.hasOwnProperty;
-
-// If we can't trust the host object, don't treat it as global
-// and fall back to inferred global through eval. In IE this
-// make a difference.
-if (window.window !== window)
-{
-    for (key in global)
-        if (hasOwnProperty.call(global, key))
-            eval(key + " = global[\"" + key + "\"];");
-}
-else
-{
-    for (key in global)
-        if (hasOwnProperty.call(global, key))
-            window[key] = global[key];
-}
