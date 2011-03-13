dc801a11c8ef5d7c6abf6a330b1ed8626f9ab608
Aparajita Fishman <aparajita@vasudevaserver.org>
2011-03-12 23:34:53 -0500

    Instead of blindly, recursively copying the AppKit and Foundation directories, more intelligently copy only the *.j files we want.

--- Tools/Documentation/make_headers.sh
+++ Tools/Documentation/make_headers.sh
@@ -10,11 +10,15 @@ if [ -d Foundation.doc ]; then
     rm -rf Foundation.doc
 fi
 
-echo "Copying source files..."
-cp -r AppKit AppKit.doc
-cp -r Foundation Foundation.doc
-
 echo "Processing source files..."
+tar cf AppKit.doc.tar --exclude='_*' -s /^AppKit/AppKit.doc/ AppKit/*.j AppKit/**/*.j
+tar xf AppKit.doc.tar
+rm AppKit.doc.tar
+
+tar cf Foundation.doc.tar --exclude='_*' -s /^Foundation/Foundation.doc/ Foundation/*.j Foundation/**/*.j
+tar xf Foundation.doc.tar
+rm Foundation.doc.tar
+
 find AppKit.doc -name *.j -exec sed -e '/@import.*/ d' -i '' {} \;
 find Foundation.doc -name *.j -exec sed -e '/@import.*/ d' -i '' {} \;
 
