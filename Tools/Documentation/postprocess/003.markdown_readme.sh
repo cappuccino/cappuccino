d4d01d9686050a8a2677dab0d73e4151ef917f9c
Alexander Ljungberg <aljungberg@wireload.net>
2011-05-14 13:09:42 -0400

    Refs #1261. Clean up README.html tmp build file when jake docs is done.

---
+++ Tools/Documentation/postprocess/003.markdown_readme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Remove the generated README.html once the build has finished.
+#
+# NOTE: The working directory should be the main capp directory when this script is run
+#
+# $1 Cappuccino documentation directory
+#
+
+rm "$1"/README.html
