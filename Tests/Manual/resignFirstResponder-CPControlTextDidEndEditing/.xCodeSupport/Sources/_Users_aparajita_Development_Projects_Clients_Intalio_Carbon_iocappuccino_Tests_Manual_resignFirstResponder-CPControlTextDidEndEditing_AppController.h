d0a5b6e1c74277be2316f5841ae37ee86feb8fd0
Aparajita Fishman <aparajita@aparajita.com>
2011-09-02 03:23:39 -0400

    Added test app.

---
+++ Tests/Manual/resignFirstResponder-CPControlTextDidEndEditing/.xCodeSupport/Sources/_Users_aparajita_Development_Projects_Clients_Intalio_Carbon_iocappuccino_Tests_Manual_resignFirstResponder-CPControlTextDidEndEditing_AppController.h
@@ -0,0 +1,7 @@
+
+@interface AppController : NSObject
+{
+
+}
+
+@end
---
+++ Tests/Manual/resignFirstResponder-CPControlTextDidEndEditing/.xCodeSupport/Sources/_Users_aparajita_Development_Projects_Clients_Intalio_Carbon_iocappuccino_Tests_Manual_resignFirstResponder-CPControlTextDidEndEditing__AppController.h
@@ -0,0 +1,10 @@
+
+@interface AppController : NSObject
+{
+    IBOutlet NSWindow* theWindow;
+    IBOutlet NSTextField* field1;
+    IBOutlet NSTextField* field2;
+    IBOutlet NSTextField* status;
+}
+
+@end
