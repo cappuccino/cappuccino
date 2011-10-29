2f7b29593e2bf704a485e5754e5016535f91505c
Sjoerd van Geffen <sjoerd.vangeffen@gmail.com>
2011-10-29 02:09:18 +0200

    replaced "280north" by "cappuccino" for github install script and docs references.
    Replaced "280north" by "cappuccino" for github username and all github
    URL references in the install script and documentation.

--- bootstrap.sh
+++ bootstrap.sh
@@ -124,7 +124,7 @@ fi
 install_directory=""
 tmp_zip="/tmp/narwhal.zip"
 
-github_user="280north"
+github_user="cappuccino"
 github_ref="master"
 tusk_install_command="install"
 
@@ -149,7 +149,7 @@ usage: ./bootstrap.sh [OPTIONS]
     --directory [DIR]:      Use a directory other than /usr/local/narwhal.
     --clone:                Do "git clone git://" instead of downloading zips.
     --clone-http:           Do "git clone http://" instead of downloading zips.
-    --github-user [USER]:   Use another github user (default: 280north).
+    --github-user [USER]:   Use another github user (default: cappuccino).
     --github-ref [REF]:     Use another git ref (default: master).
     --install-capp:         Install "objective-j" and "cappuccino" packages.
     --install-test:         Install "ojtest" package.
