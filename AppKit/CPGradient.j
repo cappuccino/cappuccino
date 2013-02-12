cappuccino/cappuccino

commit 08ad614611fd503960b8075f84540e4683d0b07c

CPGradient drawInBezierPath:angle:.

AppKit/CPGradient.j
@@ -27,6 +27,7 @@
 @import "CGColor.j"
 @import "CGColorSpace.j"
 @import "CGGradient.j"
+@import "CPBezierPath.j"
 @import "CPGraphicsContext.j"
 
 CPGradientDrawsBeforeStartingLocation   = kCGGradientDrawsBeforeStartLocation;
@@ -88,7 +89,19 @@ CPGradientDrawsAfterEndingLocation      = kCGGradientDrawsAfterEndLocation;
     CGContextClipToRect(ctx, rect);
     CGContextAddRect(ctx, rect);
 
-    var startPoint,
+    [self _drawAtAngle:angle];
+
+    CGContextRestoreGState(ctx);
+}
+
+/*!
+    @ignore like draw in rect but apply no clipping.
+*/
+- (void)_drawInRect:(CGRect)rect atAngle:(float)angle
+{
+    var ctx = [[CPGraphicsContext currentContext] graphicsPort],
+
+        startPoint,
         endPoint;
 
     // Modulo of negative values doesn't work as expected in JS.
@@ -122,7 +135,25 @@ CPGradientDrawsAfterEndingLocation      = kCGGradientDrawsAfterEndLocation;
                            startPoint.y + length * SIN(radians));
 
     [self drawFromPoint:startPoint toPoint:endPoint options:CPGradientDrawsBeforeStartingLocation | CPGradientDrawsAfterEndingLocation];
-    CGContextRestoreGState(ctx);
+}
+
+- (void)drawInBezierPath:(CPBezierPath)aPath angle:(float)anAngle
+{
+    [CPGraphicsContext saveGraphicsState];
+
+    var ctx = [[CPGraphicsContext currentContext] graphicsPort];
+
+    // Nail down the path which CGContextDrawLinearGradient will cause to be filled.
+    // Note we don't do this by clipping to the path and then calling drawInRect:atAngle:
+    // as this would cut off any antialias of the drawing, plus any active context shadow.
+    CGContextBeginPath(ctx);
+    CGContextAddPath(ctx, aPath._path);
+    CGContextSetLineWidth(ctx, [aPath lineWidth]);
+    CGContextClosePath(ctx);
+
+    [self _drawInRect:[aPath bounds] atAngle:anAngle];
+
+    [CPGraphicsContext restoreGraphicsState];
 }
 
 - (void)drawFromPoint:(NSPoint)startingPoint toPoint:(NSPoint)endingPoint options:(NSGradientDrawingOptions)options

commit 1e6c7fa6d02b26f12756cc77c72bf8c96d44fe37

Fixes #1749. Fix CPGradient error introduced by 08ad614.

AppKit/CPGradient.j
@@ -89,7 +89,7 @@ CPGradientDrawsAfterEndingLocation      = kCGGradientDrawsAfterEndLocation;
     CGContextClipToRect(ctx, rect);
     CGContextAddRect(ctx, rect);
 
-    [self _drawAtAngle:angle];
+    [self _drawInRect:rect atAngle:angle];
 
     CGContextRestoreGState(ctx);
 }