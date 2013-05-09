cappuccino/cappuccino

commit 3612565e3a7eff35e82154736bdc3c84d47c3c83

Fix delegate selection to check the index first
In the previous patch the shouldSelectRow delegate was being stored and only called once. This changes its behaviour again to first fire the delegate to see if the row in question can be selected. If not, it will look for the next row (up or down) that may be selected and choose that one.

AppKit/CPTableView.j
@@ -5236,13 +5236,18 @@ Your delegate can implement this method to avoid subclassing the tableview to ad
     if (_implementedDelegateMethods & CPTableViewDelegate_tableView_shouldSelectRow_)
     {
         var shouldSelect = [_delegate tableView:self shouldSelectRow:i];
-
-        while (!shouldSelect && (i < [self numberOfRows] && i > 0))
-            shouldGoUpward ? i-- : i++; //check to see if the row can be selected if it can't be then see if the next row can be selected
-
-        // If the index still can be selected after the loop then just return.
         if (!shouldSelect)
-            return;
+        {
+            /* If shouldSelect returns NO it means this row cannot be selected.
+                The proper behaviour is to then try to see if the next/previous
+                row(s) can be selected, until we hit the first one that can be.
+            */
+            while (!shouldSelect && (i < [self numberOfRows] && i > 0))
+            {
+                shouldGoUpward ? i-- : i++; //check to see if the row can be selected if it can't be then see if the next row can be selected
+                shouldSelect = [_delegate tableView:self shouldSelectRow:i];
+            }
+        }
     }
 
     // If we go upward and see that this row is already selected we should deselect the row below.

commit 626e1e770e7e73d6633530a5cbf4d3a40c34982b

Fixed: in the last few revisions, table rows could be selected despite delegate shouldSelectRow: returning NO.
This error was caused by 3612565e3a7eff35e82154736bdc3c84d47c3c83 dropping the return statement.

Refs #1905.

AppKit/CPTableView.j
@@ -5260,19 +5260,18 @@ Your delegate can implement this method to avoid subclassing the tableview to ad
     {
         var shouldSelect = [_delegate tableView:self shouldSelectRow:i];
 
-        if (!shouldSelect)
+        /* If shouldSelect returns NO it means this row cannot be selected.
+            The proper behaviour is to then try to see if the next/previous
+            row(s) can be selected, until we hit the first one that can be.
+        */
+        while (!shouldSelect && (i < [self numberOfRows] && i > 0))
         {
-            /* If shouldSelect returns NO it means this row cannot be selected.
-                The proper behaviour is to then try to see if the next/previous
-                row(s) can be selected, until we hit the first one that can be.
-            */
-            while (!shouldSelect && (i < [self numberOfRows] && i > 0))
-            {
-                shouldGoUpward ? --i : ++i; //check to see if the row can be selected if it can't be then see if the next row can be selected
-                shouldSelect = [_delegate tableView:self shouldSelectRow:i];
-            }
+            shouldGoUpward ? --i : ++i; //check to see if the row can be selected. If it can't be then see if the next row can be selected.
+            shouldSelect = [_delegate tableView:self shouldSelectRow:i];
         }
 
+        if (!shouldSelect)
+            return;
     }
 
     // If we go upward and see that this row is already selected we should deselect the row below.