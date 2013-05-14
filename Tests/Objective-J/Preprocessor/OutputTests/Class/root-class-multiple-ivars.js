9b709b85a965ab2e9be6277b5a8b600ea6940292
Martin Carlberg <martin@carlberg.org>
2013-05-14 10:31:29 +0200

    New: Added test cases for some preprocess directives in OutputTest.j Also fixed so OutputTest.j now uses the new compiler instead of the old.

--- Tests/Objective-J/Preprocessor/OutputTests/Class/root-class-multiple-ivars.js
+++ Tests/Objective-J/Preprocessor/OutputTests/Class/root-class-multiple-ivars.js
@@ -2,6 +2,6 @@
 var the_class = objj_allocateClassPair(Nil, "Class"),
     meta_class = the_class.isa;
 
-class_addIvars(the_class,[new objj_ivar("ivar"), new objj_ivar("array"), new objj_ivar("string"), new objj_ivar("pointer")]);
+class_addIvars(the_class,[new objj_ivar("ivar"), new objj_ivar("array"), new objj_ivar("string"), new objj_ivar("integer")]);
 
 objj_registerClassPair(the_class);
