501310c05888af1a532a9b6da9eaa21606855875
Alexander Ljungberg <aljungberg@slevenbits.com>
2013-05-16 15:24:24 -0700

    Formatting: OldBrowserCompatibility.js.
    Refs #1929.

--- Objective-J/OldBrowserCompatibility.js
+++ Objective-J/OldBrowserCompatibility.js
@@ -21,11 +21,13 @@
  */
 
 // This is for IE8 support. It doesn't have the Object.create function.
-if (!Object.create) {
-    Object.create = function(o) {
-        if (arguments.length > 1) {
+if (!Object.create)
+{
+    Object.create = function(o)
+    {
+        if (arguments.length > 1)
             throw new Error('Object.create implementation only accepts the first parameter.');
-        }
+
         function F() {}
         F.prototype = o;
         return new F();
@@ -33,70 +35,83 @@ if (!Object.create) {
 }
 
 // This is for IE8 support. It doesn't have the Object.keys function.
-if (!Object.keys) {
-  Object.keys = (function () {
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
- 
-    return function (obj) {
-      if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
- 
-      var result = [];
- 
-      for (var prop in obj) {
-        if (hasOwnProperty.call(obj, prop)) result.push(prop);
-      }
- 
-      if (hasDontEnumBug) {
-        for (var i=0; i < dontEnumsLength; i++) {
-          if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
-        }
-      }
-      return result;
-    }
-  })()
-};
+if (!Object.keys)
+{
+    Object.keys = (function ()
+    {
+        var hasOwnProperty = Object.prototype.hasOwnProperty,
+            hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+            dontEnums = [
+                'toString',
+                'toLocaleString',
+                'valueOf',
+                'hasOwnProperty',
+                'isPrototypeOf',
+                'propertyIsEnumerable',
+                'constructor'
+            ],
+            dontEnumsLength = dontEnums.length;
+
+        return function (obj)
+        {
+            if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null)
+                throw new TypeError('Object.keys called on non-object');
+
+            var result = [];
+
+            for (var prop in obj)
+            {
+                if (hasOwnProperty.call(obj, prop))
+                    result.push(prop);
+            }
+
+            if (hasDontEnumBug)
+            {
+                for (var i = 0; i < dontEnumsLength; i++)
+                {
+                    if (hasOwnProperty.call(obj, dontEnums[i]))
+                        result.push(dontEnums[i]);
+                }
+            }
+            return result;
+        };
+    })();
+}
 
 // This is for IE8 support. It doesn't have the Array.prototype.indexOf function.
-if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+if (!Array.prototype.indexOf)
+{
+    Array.prototype.indexOf = function(searchElement /*, fromIndex */ )
+    {
         "use strict";
-        if (this == null) {
+        if (this === null)
             throw new TypeError();
-        }
-        var t = Object(this);
-        var len = t.length >>> 0;
-        if (len === 0) {
+
+        var t = new Object(this),
+            len = t.length >>> 0;
+
+        if (len === 0)
             return -1;
-        }
+
         var n = 0;
-        if (arguments.length > 1) {
+        if (arguments.length > 1)
+        {
             n = Number(arguments[1]);
-            if (n != n) { // shortcut for verifying if it's NaN
+            if (n != n) // shortcut for verifying if it's NaN
                 n = 0;
-            } else if (n != 0 && n != Infinity && n != -Infinity) {
+            else if (n !== 0 && n != Infinity && n != -Infinity)
                 n = (n > 0 || -1) * Math.floor(Math.abs(n));
-            }
         }
-        if (n >= len) {
+
+        if (n >= len)
             return -1;
-        }
+
         var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
-        for (; k < len; k++) {
-            if (k in t && t[k] === searchElement) {
+        for (; k < len; k++)
+        {
+            if (k in t && t[k] === searchElement)
                 return k;
-            }
         }
         return -1;
-    }
+    };
 }
