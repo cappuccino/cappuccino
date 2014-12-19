9edb0abda0770b36cf2cb3fa1f7544525e7cba47
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-12-19 15:14:08 -0500

    Fixed: Styling is removed from build docset; Existing docset is removed before moving a new one.

--- Tools/Documentation/support/docset.sh
+++ Tools/Documentation/support/docset.sh
@@ -17,9 +17,21 @@ make
 processor_msg "Finishing up"
 mv "org.cappuccino-project.cappuccino.docset" "Cappuccino.docset"
 cp "$1"/Info.plist "Cappuccino.docset/Contents"
+
+processor_msg "Fixing CSS on Docset"
+cd Cappuccino.docset
+echo -e "open(FILE, \">>\$ARGV[0]\");\nprint FILE \"\\\n#top {display:none}\\\n#side-nav {display:none}\\\n\";\nprint \"Appended CSS to \$ARGV[0]\\\n\";\nclose(FILE);" > css.pl
+find . -type f -name "*.css" -exec perl css.pl {} \; 
+rm -rf css.pl
+
 popd
 
 processor_msg "Moving docset to Build/Documentation"
+
+if [ -d "Build/Documentation/Cappuccino.docset" ]; then
+    rm -rf "Build/Documentation/Cappuccino.docset"
+fi
+
 mv "$DOCSDIR"/Cappuccino.docset "Build/Documentation"
 
 processor_msg "Done"
