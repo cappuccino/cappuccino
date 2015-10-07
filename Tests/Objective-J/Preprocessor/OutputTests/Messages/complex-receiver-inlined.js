26a123f5a84c7a66e93315334b7c6dfc00ed3eb4
Martin Carlberg <martin@carlberg.org>
2015-10-07 15:58:28 +0200

    Fixed: Added code generation unit test cases with inlined objj_msgSend functions for all tests in OutputTest.

---
+++ Tests/Objective-J/Preprocessor/OutputTests/Messages/complex-receiver-inlined.js
@@ -0,0 +1,2 @@
+var a=0;
+((___r1=a++<2?small_target:big_target),___r1==null?null:(___r1.isa.method_msgSend["doStuff"]||_objj_forward)(___r1,"doStuff"));
---
+++ Tests/Objective-J/Preprocessor/OutputTests/Misc/parenthesis-return-inlined.js
@@ -0,0 +1,3 @@
+x=function(){
+return (someIval==null?null:(someIval.isa.method_msgSend["someMethod"]||_objj_forward)(someIval,"someMethod"));
+};
