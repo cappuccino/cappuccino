cappuccino/cappuccino

commit 586c419c46b9763408837061dd58c67281acf513

Fixed: Before this PR, Nib2Cib wasn't using theme attribute "header-view-height" (#2758)

Tools/nib2cib/NSTableHeaderView.j
@@ -38,7 +38,7 @@
         if (_bounds.size.height === 17)
         {
             var theme = [Nib2Cib defaultTheme],
-                height = [theme valueForAttributeWithName:@"default-row-height" forClass:CPTableView];
+                height = [theme valueForAttributeWithName:@"header-view-height" forClass:CPTableView];
 
             _bounds.size.height = height;
             _frame.size.height = height;

commit c0ee5f6563e14490e145df9f00484e23ca29abc7

Fixed: Problem with nib2cib introduced by #2758 (#2763)

Tools/nib2cib/NSTableHeaderView.j
@@ -23,29 +23,24 @@
 @import <AppKit/CPTableHeaderView.j>
 
 @class CPTableView
-@class Nib2Cib
-
 
 @implementation CPTableHeaderView (NSCoding)
 
 - (id)NS_initWithCoder:(CPCoder)aCoder
 {
     if (self = [super NS_initWithCoder:aCoder])
-    {
         _tableView = [aCoder decodeObjectForKey:"NSTableView"];
 
-        // change the default height
-        if (_bounds.size.height === 17)
-        {
-            var theme = [Nib2Cib defaultTheme],
-                height = [theme valueForAttributeWithName:@"header-view-height" forClass:CPTableView];
-
-            _bounds.size.height = height;
-            _frame.size.height = height;
-        }
-    }
-
     return self;
+}
+
+- (void)_setHeightIfDefaultValue:(CPInteger)height
+{
+    if (_bounds.size.height === 17)
+    {
+        _bounds.size.height = height;
+        _frame.size.height = height;
+    }
 }
 
 @end

Tools/nib2cib/NSTableView.j
@@ -86,6 +86,13 @@
         _allowsColumnReordering = (flags & 0x80000000) ? YES : NO;
 
         [self setBackgroundColor:[aCoder decodeObjectForKey:@"NSBackgroundColor"]];
+
+        var headerViewHeight = [theme valueForAttributeWithName:@"header-view-height" forClass:CPTableView];
+
+        if (!headerViewHeight)
+            headerViewHeight = [self currentValueForThemeAttribute:@"header-view-height"];
+
+        [_headerView _setHeightIfDefaultValue:headerViewHeight];
     }
 
     return self;