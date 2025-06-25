7c428bf676ded33eb5e49335159102bf80b2aed5
daboe01 <daboe01@googlemail.com>
2025-06-25 18:38:22 +0200

    try 6

---
+++ Objective-J/CommonJS/lib/objective-j.js
@@ -0,0 +1,317 @@
+
+var FILE = require("file");
+var sprintf = require("printf").sprintf;
+var OS = require("os");
+
+var window = exports.window = require("browser/window");
+
+if (system.engine === "rhino")
+{
+    window.__parent__ = null;
+    window.__proto__ = global;
+}
+
+// setup OBJJ_HOME, OBJJ_INCLUDE_PATHS, etc
+window.OBJJ_HOME = exports.OBJJ_HOME = FILE.resolve(module.path, "..");
+
+var frameworksPath = FILE.resolve(window.OBJJ_HOME, "Frameworks/");
+
+var OBJJ_INCLUDE_PATHS = global.OBJJ_INCLUDE_PATHS = exports.OBJJ_INCLUDE_PATHS = [frameworksPath];
+
+#include "Includes.js"
+
+// Find all narwhal packages with Objective-J frameworks.
+exports.objj_frameworks = [];
+exports.objj_debug_frameworks = [];
+
+var catalog = require("narwhal/packages").catalog;
+for (var name in catalog)
+{
+    if (!catalog.hasOwnProperty(name))
+        continue;
+
+    var info = catalog[name];
+    if (!info)
+        continue;
+
+    var frameworks = info["objj-frameworks"];
+    if (frameworks) {
+        if (!Array.isArray(frameworks))
+            frameworks = [String(frameworks)];
+
+        exports.objj_frameworks.push.apply(exports.objj_frameworks, frameworks.map(function(aFrameworkPath) {
+            return FILE.join(info.directory, aFrameworkPath);
+        }));
+    }
+
+    var debugFrameworks = info["objj-debug-frameworks"];
+    if (debugFrameworks) {
+        if (!Array.isArray(debugFrameworks))
+            debugFrameworks = [String(debugFrameworks)];
+
+        exports.objj_debug_frameworks.push.apply(exports.objj_debug_frameworks, debugFrameworks.map(function(aFrameworkPath) {
+            return FILE.join(info.directory, aFrameworkPath);
+        }));
+    }
+}
+
+// push to the front of the array lowest priority first.
+OBJJ_INCLUDE_PATHS.unshift.apply(OBJJ_INCLUDE_PATHS, exports.objj_frameworks);
+OBJJ_INCLUDE_PATHS.unshift.apply(OBJJ_INCLUDE_PATHS, exports.objj_debug_frameworks);
+
+if (system.env["OBJJ_INCLUDE_PATHS"])
+    OBJJ_INCLUDE_PATHS.unshift.apply(OBJJ_INCLUDE_PATHS, system.env["OBJJ_INCLUDE_PATHS"].split(":"));
+
+// bring the "window" object into scope.
+// TODO: somehow make window object the top scope?
+with (window)
+{
+// runs the objj repl or file provided in args
+exports.run = function(args)
+{
+    var multipleFiles = false;
+
+    if (args)
+    {
+        // we expect args to be in the format:
+        //  1) "objj" path
+        //  2) optional "-I" args
+        //  3) real or "virtual" main.j
+        //  4) optional program arguments
+
+        // copy the args since we're going to modify them
+        var argv = args.slice(1);
+        var outputFormatInXML = false;
+
+        if (argv[0] === "--version" || argv[0] === "-v")
+        {
+            print(exports.fullVersionString());
+            return;
+        }
+
+        if (argv[0] === "--help" || argv[0] === "-h")
+        {
+            print("Usage (objj): " + args[0].split("/").pop() + " [options] [--] files...");
+            print("  -v, --version                       print the current version of objj");
+            print("  -I, --objj-include-paths            include a specific framework paths")
+            print("  -h, --help                          print this help");
+            print("  -m, --multifiles                    launch objj on several files")
+            print("  -x, --xml                           specify the output format in xml.")
+            print("  -g, --include-debug-symbols         Include debug symbols when compiling.")
+            print("  -T, --dont-include-type-signatures  Do not include type signatures when compiling.")
+            print("  -O2, --inline-msg-send              Inline objj_msgSend function when compiling.")
+            return;
+        }
+
+        while (argv.length && argv[0].indexOf('-') === 0)
+        {
+            switch (argv[0])
+            {
+                case "--objj-include-paths":
+                case "-I":
+                    argv.shift();
+                    OBJJ_INCLUDE_PATHS.unshift.apply(OBJJ_INCLUDE_PATHS, argv.shift().split(":"));
+                    break;
+
+                case "--multifiles":
+                case "-m":
+                    argv.shift();
+                    multipleFiles = true;
+                    break
+
+                case "-x":
+                case "--xml":
+                    argv.shift();
+                    exports.messageOutputFormatInXML = true;
+                    outputFormatInXML = true;
+                    break;
+
+                case "-g":
+                case "--include-debug-symbols":
+                    argv.shift();
+                    var flags = ObjectiveJ.FileExecutable.currentCompilerFlags();
+                    flags.includeMethodFunctionNames = true;
+                    ObjectiveJ.FileExecutable.setCurrentCompilerFlags(flags);
+                    break;
+
+                case "-T":
+                case "--dont-include-type-signatures":
+                    argv.shift();
+                    var flags = ObjectiveJ.FileExecutable.currentCompilerFlags();
+                    flags.includeIvarTypeSignatures = true;
+                    flags.includeMethodArgumentTypeSignatures = true;
+                    ObjectiveJ.FileExecutable.setCurrentCompilerFlags(flags);
+                    break;
+
+                case "-O2":
+                case "--inline-msg-send":
+                    argv.shift();
+                    var flags = ObjectiveJ.FileExecutable.currentCompilerFlags();
+                    flags.inlineMsgSendFunctions = true;
+                    ObjectiveJ.FileExecutable.setCurrentCompilerFlags(flags);
+                    break;
+
+                case "-g":
+                case "--include-debug-symbols":
+                    argv.shift();
+                    var flags = ObjectiveJ.FileExecutable.currentCompilerFlags();
+                    flags.includeMethodFunctionNames = true;
+                    ObjectiveJ.FileExecutable.setCurrentCompilerFlags(flags);
+                    break;
+
+                case "-T":
+                case "--dont-include-type-signatures":
+                    argv.shift();
+                    var flags = ObjectiveJ.FileExecutable.currentCompilerFlags();
+                    flags.includeIvarTypeSignatures = true;
+                    flags.includeMethodArgumentTypeSignatures = true;
+                    ObjectiveJ.FileExecutable.setCurrentCompilerFlags(flags);
+                    break;
+
+                case "-O2":
+                case "--inline-msg-send":
+                    argv.shift();
+                    var flags = ObjectiveJ.FileExecutable.currentCompilerFlags();
+                    flags.inlineMsgSendFunctions = true;
+                    ObjectiveJ.FileExecutable.setCurrentCompilerFlags(flags);
+                    break;
+
+                default:
+                    print(args[0].split("/").pop() + " illegal option " + argv[0]);
+                    OS.exit(1);
+            }
+        }
+    }
+
+    if (argv && argv.length > 0)
+    {
+        var endCommand = false,
+            errors = [];
+
+        while (argv.length > 0)
+        {
+            var arg0 = argv.shift(),
+                mainFilePath = FILE.canonical(arg0);
+
+            try
+            {
+                exports.make_narwhal_factory(mainFilePath)(require, { }, module, system, print);
+            }
+            catch(e)
+            {
+                errors.push(e);
+            }
+
+            if (typeof main === "function")
+            {
+                main([arg0].concat(argv));
+                endCommand = true;
+            }
+
+            require("browser/timeout").serviceTimeouts();
+
+            if (!multipleFiles || endCommand)
+                break;
+
+            ObjectiveJ.Executable.resetCachedFileExecutableSearchers();
+            ObjectiveJ.StaticResource.resetRootResources();
+            ObjectiveJ.FileExecutable.resetFileExecutables();
+            objj_resetRegisterClasses();
+        }
+
+        if (errors.length)
+        {
+            // Make sure we get exit will failiure
+            OS.exit(1);
+        }
+    }
+    else
+    {
+        exports.repl();
+    }
+}
+
+exports.repl = function()
+{
+    var READLINE = require("readline");
+
+    var historyPath = FILE.path(system.env["HOME"], ".objj_history");
+    var historyFile = null;
+
+    if (historyPath.exists() && READLINE.addHistory) {
+        historyPath.read({ charset : "UTF-8" }).split("\n").forEach(function(line) {
+            if (line.trim())
+                READLINE.addHistory(line);
+        });
+    }
+
+    try {
+        historyFile = historyPath.open("a", { charset : "UTF-8" });
+    } catch (e) {
+        system.stderr.print("Warning: Can't open history file '"+historyFile+"' for writing.");
+    }
+
+    while (true)
+    {
+        try {
+            system.stdout.write("objj> ").flush();
+
+            var line = READLINE.readline();
+            if (line && historyFile)
+                historyFile.write(line).write("\n").flush();
+
+            var result = exports.objj_eval(line);
+            if (result !== undefined)
+                print(result);
+
+        } catch (e) {
+            print(e);
+        }
+
+        require("browser/timeout").serviceTimeouts();
+    }
+};
+
+// creates a narwhal factory function in the objj module scope
+exports.make_narwhal_factory = function(path, basePath, filenameTranslateDictionary)
+{
+    return function(require, exports, module, system, print)
+    {
+        Executable.setCommonJSParameters("require", "exports", "module", "system", "print", "window");
+        Executable.setCommonJSArguments(require, exports, module, system, print, window);
+        filenameTranslateDictionary && Executable.setFilenameTranslateDictionary(filenameTranslateDictionary);
+        Executable.fileImporterForURL(basePath ? basePath : FILE.dirname(path))(path, YES);
+    }
+};
+
+} // end "with"
+
+var pkg = null;
+function getPackage() {
+    if (!pkg)
+        pkg = JSON.parse(FILE.path(module.path).dirname().dirname().join("package.json").read({ charset : "UTF-8" }));
+    return pkg;
+}
+
+exports.version = function() { return getPackage()["version"]; }
+exports.revision = function() { return getPackage()["cappuccino-revision"]; }
+exports.timestamp = function() { return new Date(getPackage()["cappuccino-timestamp"]); }
+
+exports.fullVersionString = function() {
+    return sprintf("objective-j %s (%04d-%02d-%02d %s)",
+        exports.version(),
+        exports.timestamp().getUTCFullYear(),
+        exports.timestamp().getUTCMonth()+1,
+        exports.timestamp().getUTCDate(),
+        exports.revision().slice(0,6)
+    );
+}
+
+global.ObjectiveJ = {};
+
+for (var key in exports)
+    if (Object.prototype.hasOwnProperty.call(exports, key))
+        global.ObjectiveJ[key] = exports[key];
+
+if (require.main == module.id)
+    exports.run(system.args);
---
+++ Objective-J/CommonJS/lib/objective-j/compiler.js
@@ -0,0 +1,317 @@
+
+var FILE = require("file"),
+    OS = require("os"),
+    ObjectiveJ = require("objective-j"),
+    JAKE = require("jake");
+
+require("objective-j/rhino/regexp-rhino-patch");
+
+var compressors = {
+    ss  : { id : "minify/shrinksafe" }
+    //,yui : { id : "minify/yuicompressor" }
+    // ,cc  : { id : "minify/closure-compiler" }
+};
+
+var compressorStats = {};
+function compressor(code)
+{
+    var winner, winnerName;
+    compressorStats['original'] = (compressorStats['original'] || 0) + code.length;
+    for (var name in compressors)
+    {
+        var aCompressor = require(compressors[name].id),
+            result = aCompressor.compress(code, { charset : "UTF-8", useServer : true });
+        compressorStats[name] = (compressorStats[name] || 0) + result.length;
+        if (!winner || result < winner.length) {
+            winner = result;
+            winnerName = name;
+        }
+    }
+    // print("winner="+winnerName+" compressorStats="+JSON.stringify(compressorStats));
+    return winner;
+}
+
+function compileWithResolvedFlags(aFilePath, objjcFlags, gccFlags, asPlainJavascript)
+{
+    var shouldObjjPreprocess = true,
+        shouldCompress = objjcFlags.compress,
+        fileContents = "",
+        executable,
+        code;
+
+    if (!shouldObjjPreprocess)
+    {
+        try
+        {
+            var p = OS.popen("which gcc");
+
+            if (p.stdout.read().length === 0)
+            {
+                fileContents = FILE.read(aFilePath, { charset:"UTF-8" });
+            }
+            else
+            {
+                try
+                {
+                    var gcc = OS.popen("gcc -E -x c -P " + (gccFlags ? gccFlags.join(" ") : "") + " " + OS.enquote(aFilePath), { charset:"UTF-8" }),
+                        chunk = "";
+
+                    while (chunk = gcc.stdout.read())
+                        fileContents += chunk;
+                }
+                finally
+                {
+                    gcc.stdin.close();
+                    gcc.stdout.close();
+                    gcc.stderr.close();
+                }
+            }
+        }
+        finally
+        {
+            p.stdin.close();
+            p.stdout.close();
+            p.stderr.close();
+        }
+
+        return fileContents;
+    }
+
+    // Preprocess contents into fragments.
+    // FIXME: should calculate relative path, etc.
+    try
+    {
+        var sources = new JAKE.FileList("**/*.j"),
+            translateFilenameToPath = {},
+            otherwayTranslateFilenameToPath = {};
+
+        // Create a filename to filename path dictionary. (For example: CPArray.j -> CPArray/CPArray.j)
+        sources.forEach(function(/*String*/ aFilename)
+        {
+            translateFilenameToPath[FILE.basename(aFilename)] = aFilename;
+            otherwayTranslateFilenameToPath[aFilename] = FILE.basename(aFilename);
+        }, this);
+
+        var translatedFilename = translateFilenameToPath[aFilePath] ? translateFilenameToPath[aFilePath] : aFilePath,
+            otherwayTranslatedFilename = otherwayTranslateFilenameToPath[aFilePath] ? otherwayTranslateFilenameToPath[aFilePath] : aFilePath,
+            theTranslatedFilename = otherwayTranslatedFilename ? otherwayTranslatedFilename : translatedFilename,
+            absolutePath = FILE.absolute(theTranslatedFilename),
+            basePath = absolutePath.substring(0, absolutePath.length - theTranslatedFilename.length);
+
+        // This ugly fix to make Cappuccino compile with rhino can be removed when the compiler is not loading classes into the runtime
+        var rhinoUglyFix = false;
+        if (system.engine === "rhino")
+        {
+            if (typeof document == "undefined") {
+                document = { createElement: function(x) { return { innerText: "", style: {}}}};
+                rhinoUglyFix = true;
+            }
+            if (typeof navigator == "undefined")
+            {
+                navigator =  { "userAgent": "fakenavigator" };
+                rhinoUglyFix = true;
+            }
+        }
+
+        ObjectiveJ.FileExecutable.setCurrentCompilerFlags(objjcFlags);
+        ObjectiveJ.make_narwhal_factory(absolutePath, basePath, translateFilenameToPath)(require, {}, module, system, print);
+
+        executable = new ObjectiveJ.FileExecutable(FILE.basename(aFilePath));
+
+        if (rhinoUglyFix)
+            delete document;
+    }
+    catch (anException)
+    {
+        print(anException);
+        return;
+    }
+
+    if (shouldCompress)
+    {
+        code = executable.code();
+        code = compressor("function(){" + code + "}");
+        // more robust function wrapper stripping
+        code = code.replace(/^\s*function\s*\(\s*\)\s*{|}\s*;?\s*$/g, "");
+        executable.setCode(code);
+    }
+
+    return asPlainJavascript ? executable.code() : executable.toMarkedString();
+}
+
+function resolveFlags(args)
+{
+    var filePaths = [],
+        outputFilePaths = [],
+
+        index = 0,
+        count = args.length,
+
+        gccFlags = [],
+        objjcFlags = {};
+
+    for (; index < count; ++index)
+    {
+        var argument = args[index];
+
+        if (argument === "-o")
+        {
+            if (++index < count)
+                outputFilePaths.push(args[index]);
+        }
+
+        else if (argument.indexOf("-D") === 0)
+            gccFlags.push(argument);
+
+        else if (argument.indexOf("-U") === 0)
+            gccFlags.push(argument);
+
+        else if (argument === "--include")
+        {
+            if (++index < count)
+            {
+                gccFlags.push(argument);
+                gccFlags.push(args[index]);
+            }
+        }
+
+        else if (argument.indexOf("-T") === 0)
+        {
+            objjcFlags.includeIvarTypeSignatures = false;
+            objjcFlags.includeMethodArgumentTypeSignatures = false;
+        }
+        else if (argument.indexOf("-g") === 0)
+            objjcFlags.includeMethodFunctionNames = true;
+
+        else if (argument.indexOf("-O") === 0) {
+            objjcFlags.compress = true;
+            // FIXME: currently we are sending in '-O2' when we want InlineMsgSend. Here we only check if we it is '-O...'
+            if (argument.length > 2)
+                objjcFlags.inlineMsgSendFunctions = true;
+        }
+
+        else if (argument.indexOf("-G") === 0)
+            objjcFlags.generate = true;
+
+        else if (argument.indexOf("--inline-msg-send") === 0)
+        {
+            // This option is if you only want to inline message send functions and not compress
+            objjcFlags.inlineMsgSendFunctions = true;
+        }
+        else
+            filePaths.push(argument);
+    }
+
+    return { filePaths:filePaths, outputFilePaths:outputFilePaths, objjcFlags:objjcFlags, gccFlags:gccFlags };
+}
+
+exports.compile = function(aFilePath, flags)
+{
+    if (flags.split)
+        flags = flags.split(/\s+/);
+
+    var resolvedFlags = resolveFlags(flags);
+
+    return compileWithResolvedFlags(aFilePath, resolvedFlags.objjcFlags, resolvedFlags.gccFlags);
+};
+
+exports.main = function(args)
+{
+    var shouldPrintOutput = false,
+        asPlainJavascript = false,
+        objjcFlags = {};
+
+    var argv = args.slice(1);
+
+    while(argv.length)
+    {
+        if (argv[0] === '--')
+        {
+            argv.shift();
+            break;
+        }
+
+        if (argv[0] === "-p" || argv[0] === "--print")
+        {
+            shouldPrintOutput = YES;
+            argv.shift();
+            continue;
+        }
+
+        if (argv[0] === "--unmarked")
+        {
+            asPlainJavascript = true;
+            argv.shift();
+            continue;
+        }
+
+        if (argv[0] === "-T" || argv[0] === "--includeTypeSignatures")
+        {
+            objjcFlags.includeIvarTypeSignatures = true;
+            objjcFlags.includeMethodArgumentTypeSignatures = true;
+            argv.shift();
+            continue;
+        }
+
+        if (argv[0] === "--help" || argv[0].substr(0, 1) == '-')
+        {
+            print("Usage (objjc 2.0): " + args[0] + " [options] [--] file...");
+            print("  -p, --print                                  print the output directly to stdout");
+            print("  --unmarked                                   don't tag the output with @STATIC header");
+            print("");
+            print("  -T, --dont-include-type-signatures           include type signatures in the compiled output");
+            print("  -g, --include-debug-symbols                  include debug symbols in the compiled output");
+            print("  -T, --include-type-signatures                include type signatures in the compiled output");
+            print("  -O, --compress                               compress the compiled output");
+            print("  -O2, --inline-msg-send                       inline objj_msgSend function in the compiled output");
+            print("  -Wno-unused-but-set-variable                 turn off warning when a local variable is never read");
+            print("  -Wno-shadow-ivar                             turn off warning when a local variable is shadowing an instance variable for the class");
+            print("  -Wno-create-global-inside-function-or-method turn off warning when creating a global variable inside a function or method");
+            print("  -Wno-unknown-class-or-global.                turn off warning when a class or global variable is not known");
+            print("  -Wno-unknown-ivar-type                       turn off warning when the type for an instance variable is not known");
+            print("  To turn on a warning flag remove the 'no-' prefix. Example: -Wunused-but-set-variable");
+            print("");
+            print("  --help                              print this help");
+            return;
+        }
+
+        // Current argument doesn't begin with - so it's not an argument but the
+        // first filename.
+        // TODO Full GNU getopt parsing which doesn't stop on the first non-argument.
+        break;
+    }
+
+    var resolved = resolveFlags(argv),
+        outputFilePaths = resolved.outputFilePaths,
+        gccFlags = resolved.gccFlags,
+        resolvedObjjcFlags = resolved.objjcFlags;
+
+    // Merge resolved keys into objjcFlags
+    for (var key in resolvedObjjcFlags)
+    {
+        if (resolvedObjjcFlags.hasOwnProperty(key))
+        {
+            if (resolvedObjjcFlags[key])
+                objjcFlags[key] = resolvedObjjcFlags[key];
+        }
+    }
+
+    resolved.filePaths.forEach(function(filePath, index)
+    {
+        if (!shouldPrintOutput)
+            print("Statically Compiling " + filePath);
+        var output = compileWithResolvedFlags(filePath, objjcFlags, gccFlags, asPlainJavascript);
+
+        // Only write the output if the compilation didn't fail.
+        if (output !== undefined)
+        {
+            if (shouldPrintOutput)
+                print(output);
+            else
+                FILE.write(outputFilePaths[index], output, { charset: "UTF-8" });
+        }
+    });
+};
+
+if (require.main == module.id)
+    exports.main(system.args);
