b62721d79ae7a7cf84f323c5dc038f7b7a8fa620
David Richardson <david.richardson@enquora.com>
2026-08-05 13:47:28 -0600

    Remove _IS_NUMERIC macro in favor of concrete funtion CPIsNumeric()
    - Create Foundation/_CPFoundationUtilities.j to house global low-level utilities.
    - Implement CPIsNumeric as a concrete JavaScript function, preserving the exact legacy logic.
    - Import _CPFoundationUtilities.j in the Foundation.j umbrella manifest.
    - Replace _IS_NUMERIC macro calls at all call sites with CPIsNumeric().

--- AppKit/CPStackView.j
+++ AppKit/CPStackView.j
@@ -20,8 +20,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "../Foundation/Foundation.h"
-
 @import "CPView.j"
 
 // Gravity Areas
--- Foundation/Foundation.h
+++
@@ -1,2 +0,0 @@
-// By Christian C. Salvadó, http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric/1830844#1830844
-#define _IS_NUMERIC(n) (!isNaN(parseFloat(n)) && isFinite(n))
